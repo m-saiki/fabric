05b059e541ffba6fde826eff0dd97e7842202992
Mio Saiki <m-saiki@adminnoMacBook-Air.local>
2019-11-26 16:50:04 +0900

    first commit

---
+++ chaincode/encfabcar/javascript/index.js
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const EncFabCar = require('./lib/encfabcar');
+
+
+module.exports.EncFabCar = EncFabCar;
+module.exports.contracts = [ EncFabCar ];
